0340c1da081a509859f494550bae63adb6939725
dyu <david.yu.ftw@gmail.com>
2017-07-09 02:06:37 +0800

    type

--- todo-ts/src/context.ts
+++ todo-ts/src/context.ts
@@ -1,6 +1,7 @@
 import { PojoStore } from 'coreds/lib/pstore/'
+import { user } from '../g/user/'
 
 export interface Stores {
-    todo: PojoStore<any>
+    todo: PojoStore<user.Todo>
 }
 export const stores: Stores = {} as Stores
