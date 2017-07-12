f54cbed581319fdb7ed783fe09213ba01bd4da45
dyu <david.yu.ftw@gmail.com>
2017-07-12 14:50:06 +0800

    ordering, remove unused

--- todo-ts/src/main.ts
+++ todo-ts/src/main.ts
@@ -10,13 +10,12 @@ for (var i of Object.keys(filters)) Vue.filter(i, filters[i])
 
 // directives
 Vue.directive('append', require('coreds-ui/lib/d2/append'))
+Vue.directive('clear', require('coreds-ui/lib/d2/clear'))
 Vue.directive('defp', require('coreds-ui/lib/d2/defp'))
+Vue.directive('dpicker', require('coreds-ui/lib/d2/dpicker'))
 Vue.directive('pager', require('coreds-ui/lib/d2/pager'))
 Vue.directive('sval', require('coreds-ui/lib/d2/sval'))
 Vue.directive('toggle', require('coreds-ui/lib/d2/toggle'))
-Vue.directive('clear', require('coreds-ui/lib/d2/clear'))
-Vue.directive('suggest', require('coreds-ui/lib/d2/suggest'))
-Vue.directive('dpicker', require('coreds-ui/lib/d2/dpicker'))
 
 let app = require('./App.vue')
 new Vue(app).$mount(document.getElementById(TARGET) as any)
