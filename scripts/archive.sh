ec974f775476583b976b42f6527cbd0e837d6b87
dyu <david.yu.ftw@gmail.com>
2017-08-03 21:24:45 +0800

    nw executable

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -7,8 +7,13 @@ ZIP_FILE=target/todo-win-x64.zip
 
 echo "========== tar.gz"
 rm -f $TAR_FILE
-tar -cvzf $TAR_FILE target/protostuffdb -T scripts/files.txt
+cp target/opt-nw todo-ts/todo
+tar -cvzf $TAR_FILE target/hprotostuffdb-rjre target/jre todo-ts/todo -T scripts/files.txt
+rm -f todo-ts/todo
 
 echo "\n========== zip"
 rm -f $ZIP_FILE
-zip -r $ZIP_FILE target/protostuffdb.exe todo-ts/node-chrome-app.vbs -@ < scripts/files.txt
+cp target/opt-nw.exe todo-ts/todo.exe
+zip -r $ZIP_FILE target/protostuffdb.exe todo-ts/todo.exe -@ < scripts/files.txt
+rm -f todo-ts/todo.exe
+
