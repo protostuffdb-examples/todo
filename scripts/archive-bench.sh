0900c0a35db57bdfaf23b0df27cd98f20f5cda8d
dyu <david.yu.ftw@gmail.com>
2017-10-09 19:00:05 +0800

    archive bench

---
+++ scripts/archive-bench.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+[ -e scripts ] || { echo 'Execute this script from root dir.'; exit 1; }
+
+TAR_FILE=target/todo-linux-bench-x64.tar.gz
+ZIP_FILE=target/todo-win-bench-x64.zip
+
+COMMON_FILES='ARGS.txt PORT.txt todo-ts/g/user/UserServices.json bench/README.md bench/run.properties bench/run-uri.sh bench/payload'
+
+echo "========== tar.gz"
+rm -f $TAR_FILE
+tar -cvzf $TAR_FILE target/hprotostuffdb-bench target/protostuffdb-bench $COMMON_FILES
+
+echo "\n========== zip"
+rm -f $ZIP_FILE
+zip -r $ZIP_FILE target/protostuffdb-bench.exe $COMMON_FILES
