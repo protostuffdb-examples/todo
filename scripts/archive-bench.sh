19a141001d9f6b00c7d5f6e0bab7bc92de6cda26
dyu <david.yu.ftw@gmail.com>
2017-10-12 12:29:46 +0800

    standalone and variants

--- scripts/archive-bench.sh
+++ scripts/archive-bench.sh
@@ -3,13 +3,18 @@
 [ -e scripts ] || { echo 'Execute this script from root dir.'; exit 1; }
 
 TAR_FILE=target/todo-linux-bench-x64.tar.gz
+S_TAR_FILE=target/todo-linux-bench-standalone-x64.tar.gz
 ZIP_FILE=target/todo-win-bench-x64.zip
 
 COMMON_FILES='ARGS.txt PORT.txt todo-all/target/todo-all-jarjar.jar todo-ts/g/user/UserServices.json bench/README.md bench/run.properties bench/run.sh bench/run-uri.sh bench/payload'
 
 echo "========== tar.gz"
 rm -f $TAR_FILE
-tar -cvzf $TAR_FILE target/protostuffdb-bench target/hprotostuffdb-bench target/hprotostuffdb-rmaster-bench  $COMMON_FILES
+tar -cvzf $TAR_FILE target/protostuffdb-bench target/protostuffdb-rmaster-bench target/hprotostuffdb-bench target/hprotostuffdb-rmaster-bench  $COMMON_FILES
+
+echo "========== standalone tar.gz"
+rm -f $S_TAR_FILE
+tar -cvzf $S_TAR_FILE target/jre/*/ target/protostuffdb-rjre-bench target/protostuffdb-rmaster-rjre-bench target/hprotostuffdb-rjre-bench target/hprotostuffdb-rmaster-rjre-bench  $COMMON_FILES
 
 echo "\n========== zip"
 rm -f $ZIP_FILE
