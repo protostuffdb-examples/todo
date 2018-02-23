29a46252c6f91f6e4f68594a1de18cd8779a4986
dyu <david.yu.ftw@gmail.com>
2018-02-23 18:47:29 +0800

    trailing slash

--- todo-cpp/src/util.h
+++ todo-cpp/src/util.h
@@ -53,10 +53,13 @@ const char* resolveEndpoint(char* arg, int* port, bool* secure)
     
     if (':' != *(slash - 1))
     {
-        // assume it is a trailing slash
+        // expect a trailing slash
+        if ('\0' != *(slash + 1))
+            return nullptr;
+        
         *secure = false;
         *slash = '\0';
-        return '\0' != *(slash + 1) ? nullptr : resolveIpPort(arg, port); 
+        return resolveIpPort(arg, port); 
     }
     
     if ('/' != *(slash + 1))
