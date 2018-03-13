81e654972dbfdba0fa19879d247140d050110974
dyu <david.yu.ftw@gmail.com>
2018-03-13 18:36:09 +0800

    scripts

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -2,18 +2,35 @@
 
 [ -e scripts ] || { echo 'Execute this script from root dir.'; exit 1; }
 
-TAR_FILE=target/todo-linux-x64.tar.gz
-ZIP_FILE=target/todo-win-x64.zip
+OUT_DIR=target/bin
+
+[ -e $OUT_DIR ] || mkdir -p $OUT_DIR
+
+TAR_FILE=$OUT_DIR/todo-linux-x64.tar.gz
+ZIP_FILE=$OUT_DIR/todo-win-x64.zip
+
+printf todo > target/name.txt
 
 echo "========== tar.gz"
 rm -f $TAR_FILE
 cp target/opt-nw todo-ts/todo
-tar -cvzf $TAR_FILE target/protostuffdb todo-ts/todo -T scripts/files.txt
+tar -cvzf $TAR_FILE target/name.txt target/protostuffdb todo-ts/todo -T scripts/files.txt
 rm -f todo-ts/todo
 
 echo "\n========== zip"
 rm -f $ZIP_FILE
 cp target/opt-nw.exe todo-ts/todo.exe
-zip -r $ZIP_FILE target/protostuffdb.exe todo-ts/todo.exe -@ < scripts/files.txt
+zip -r $ZIP_FILE target/name.txt target/protostuffdb.exe todo-ts/todo.exe -@ < scripts/files.txt
 rm -f todo-ts/todo.exe
 
+# desktop executable
+cp todo-cpp/gn-out/todo target/todo
+
+TAR_FILE=$OUT_DIR/todo-client-linux-x64.tar.gz
+
+rm -f $TAR_FILE
+cp target/setup .
+tar -cvzf $TAR_FILE setup target/todo target/name.txt assets/*
+rm -f setup
+
+# TODO zipfile
