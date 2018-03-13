7f744dc3d55bd80a3b2af485c0c04a650f1fd0f8
dyu <david.yu.ftw@gmail.com>
2018-03-13 19:28:59 +0800

    check target/ARGS.txt

--- todo-cpp/src/main.cpp
+++ todo-cpp/src/main.cpp
@@ -1,8 +1,71 @@
 #include "app.h"
+#include <string>
+#include <vector>
+#include <fstream>
+
+static bool isSPCRLF(char c)
+{
+    return c == ' ' || c == '\r' || c == '\n';
+}
+
+static bool LoadFile(const char *name, bool binary, std::string *buf, bool stripWS = false)
+{
+    std::ifstream ifs(name, binary ? std::ifstream::binary : std::ifstream::in);
+    if (!ifs.is_open())
+        return false;
+    *buf = std::string(std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>());
+    if (stripWS && !buf->empty())
+    {
+        while (isSPCRLF(buf->back()))
+            buf->pop_back();
+    }
+    return !ifs.bad();
+}
+
+bool find_space(std::string& args, std::vector<size_t>& offsets)
+{
+    for (size_t idx = 0, len = args.size();
+            idx < len && std::string::npos != (idx = args.find(' ', idx));
+            idx++)
+    {
+        offsets.push_back(idx);
+        args[idx] = '\0';
+    }
+    
+    return !offsets.empty();
+}
 
 int main(int argc, char* argv[])
 {
-    //char* def_args[] = { "todo", "https://45.32.108.204", "todo.dyuproject.com" };
-    //return argc == 1 ? todo::run(3, def_args) : todo::run(argc, argv);
-    return todo::run(argc, argv);
+    int ret;
+    std::string args;
+    std::vector<size_t> offsets;
+    if (argc > 1 || !LoadFile("target/ARGS.txt", false, &args, true))
+    {
+        ret = todo::run(argc, argv);
+    }
+    else if (!find_space(args, offsets))
+    {
+        char* argv_override[]{ argv[0], const_cast<char*>(args.c_str()), nullptr };
+        ret = todo::run(2, argv_override);
+    }
+    else
+    {
+        size_t i = 0, start = 0, count = 1 + 1 + offsets.size();
+        char** argv_override = new char*[count + 1];
+        
+        argv_override[count] = nullptr;
+        argv_override[i++] = argv[0];
+        for (auto idx : offsets)
+        {
+            argv_override[i++] = const_cast<char*>(args.data() + start);
+            start = idx + 1;
+        }
+        argv_override[i++] = const_cast<char*>(args.data() + start);
+        
+        ret = todo::run(static_cast<int>(count), argv_override);
+        
+        delete[] argv_override;
+    }
+    return ret;
 }
