bba8e3f4af0f05aabd6a5244995119c17f99fa32
dyu <david.yu.ftw@gmail.com>
2018-03-13 18:10:54 +0800

    desktop

--- scripts/s-archive.sh
+++ scripts/s-archive.sh
@@ -6,13 +6,25 @@ OUT_DIR=target/bin
 
 [ -e $OUT_DIR ] || mkdir -p $OUT_DIR
 
-TAR_FILE=$OUT_DIR/todo-standalone-linux-x64.tar.gz
+TAR_FILE=$OUT_DIR/todo-server-standalone-linux-x64.tar.gz
 
-TAR_ARGS='target/protostuffdb-rjre target/jre/*/'
+TAR_ARGS='target/name.txt target/protostuffdb-rjre target/jre/*/'
 [ "$1" != "" ] && TAR_ARGS=$1
+
+printf todo > target/name.txt
 
 echo "========== tar.gz"
 rm -f $TAR_FILE
 echo '#!/bin/sh' > start.sh && tail --lines=+4 scripts/s-start.sh >> start.sh && chmod +x start.sh && \
     tar -cvzf $TAR_FILE start.sh $TAR_ARGS -T scripts/files.txt
 rm start.sh
+
+# desktop files
+head --lines=5 scripts/files.txt > target/files.txt
+cp todo-cpp/gn-out/todo-pdb target/todo
+
+TAR_FILE=$OUT_DIR/todo-desktop-standalone-linux-x64.tar.gz
+rm -f $TAR_FILE
+cp target/setup .
+tar -cvzf $TAR_FILE setup target/todo assets/* $TAR_ARGS -T target/files.txt
+rm setup
