5d6d08c35eca1dd573c0bbd2c3baa0461d5f28a3
dyu <david.yu.ftw@gmail.com>
2022-02-17 21:19:57 +0800

    formatting

--- todo-ts/test-e2e.sh
+++ todo-ts/test-e2e.sh
@@ -17,4 +17,5 @@ fi
 cd $SCRIPT_DIR
 
 PORT=$(cat ../PORT.txt)
-RPC_HOST="http://127.0.0.1:$PORT" node --experimental-fetch ./node_modules/.bin/uvu -r tsm -r esm test/uvu
+RPC_HOST="http://127.0.0.1:$PORT" node --experimental-fetch \
+./node_modules/.bin/uvu -r tsm -r esm test/uvu
